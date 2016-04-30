577aa933d2efb2f0a01aaf32ea4c5d5c03ba76a1
bowonKang <skykbw@hanmail.net>
2016-04-30 16:15:07 +0900

    재귀를 이용한 순열

---
+++ Algorithm/src/BackTracking/Permutation.java
@@ -0,0 +1,39 @@
+package BackTracking;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Permutation {
+
+     static final int numLength = 4;
+     
+     static int[] numArr = new int[numLength];
+     static boolean[] isUsedNum = new boolean[numLength];    
+     
+     public static void main(String[] args) throws Exception {
+         getPermutation(0);
+     }
+
+     private static void getPermutation(int num) {
+          if(num == numLength){
+             for(int i=0; i<numArr.length; i++){
+                 System.out.print(numArr[i] + " ");
+             }
+             System.out.println();
+             return;
+         }
+         
+         //숫자 배열을 탐색하며 값이 비어있을경우 넣고 숫자를 증가시켜 재귀호출        
+         //재귀호출이 완료되면 다시 값을 지워주고 다음숫자로 넘어간다
+        for(int i=0; i<numLength; i++){
+             if(isUsedNum[i] == false){
+                 numArr[num] = i+1;
+                 isUsedNum[i] = true;
+                 getPermutation(num+1);
+                 
+                 isUsedNum[i] = false;            
+             }         
+         }            
+     }      
+}
+
